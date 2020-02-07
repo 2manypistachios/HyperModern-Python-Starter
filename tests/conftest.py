989878b741d0c4045eaea3b591aeff01e0b6450f
2manypistachios <2manypistachios@gmail.com>
2020-02-07 16:56:27 -0500

    Documentation Fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-# Easy Grouping of Fixtures
+"""Package-wide test fixtures."""
 # Standard
 from unittest.mock import Mock
 
@@ -9,11 +9,13 @@ from pytest_mock import MockFixture
 
 
 def pytest_configure(config: Config) -> None:
+    """Pytest configuration hook."""
     config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
 
 
 @pytest.fixture
 def mock_requests_get(mocker: MockFixture) -> Mock:
+    """Fixture for mocking requests.get."""
     mock = mocker.patch("requests.get")
     mock.return_value.__enter__.return_value.json.return_value = {
         "title": "Lorem Ipsum",
